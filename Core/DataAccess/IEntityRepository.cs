2c531bafcf46fda5686abc5b7b5a714e54c395c4
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-15 00:29:15 +0300

    veritabanı bağlantısı sağlandı

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Abstract
    public  interface IEntityRepository<T> where T:class,IEntity,new()
     {
 
-        List<T> GetAll(Expression<Func<T,bool>> filter);
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T,bool>> filter);
         void Add(T entity);
         void Update(T entity);
